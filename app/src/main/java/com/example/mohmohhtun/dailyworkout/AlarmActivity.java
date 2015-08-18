06477294837323e62776bca50297bdf4034601e2
mohmohhtun <shmohmoh75@gmail.com>
2015-08-18 15:54:09 +0800

    updated version1.0.0

--- app/src/main/java/com/example/mohmohhtun/dailyworkout/AlarmActivity.java
+++ app/src/main/java/com/example/mohmohhtun/dailyworkout/AlarmActivity.java
@@ -5,28 +5,18 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.DatePicker;
 import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
-import android.widget.ToggleButton;
-
 import java.util.Calendar;
 
 
 public class AlarmActivity extends Activity {
 
-    DatePicker pickerDate;
     TimePicker pickerTime;
     Switch alarmToggle;
-    TextView info;
     Calendar current;
     Calendar calendar;
     final static int RQS_1 = 1;
@@ -50,7 +40,6 @@ public class AlarmActivity extends Activity {
 
                     calendar.set(Calendar.HOUR_OF_DAY, pickerTime.getCurrentHour());
                     calendar.set(Calendar.MINUTE, pickerTime.getCurrentMinute());
-                    Calendar cal = Calendar.getInstance();
 
                     if (calendar.compareTo(current) <= 0) {
 //                        The set Date/Time already passed
